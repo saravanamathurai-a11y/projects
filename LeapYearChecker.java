060e9db69ceabec9becfd9454dc3c7fe91d1a2be
saravanamathurai-a11y <saravanamathurai@gmail.com>
2025-08-23 10:51:09 +0530

    Add files via upload

---
+++ LeapYearChecker.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeap = false;
+
+        if (year % 4 == 0) {
+            isLeap = true;
+        }
+
+        if (isLeap) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+}
